b17aac3af98e73c770268a55f631a51c38153d7a
Olirpo <olvatgamer@gmail.com>
2021-06-05 20:03:00 -0300

    Primer version

---
+++ functions.py
@@ -0,0 +1,12 @@
+def brute_force(goal):
+    x = 0
+
+    while x**2<goal:
+        x += 1
+    if x**2 == goal:
+        print(f'The square root of {goal} is {x}')
+    else:
+        print(f'I couldnt find the root')
+
+def aproximation:
+    
