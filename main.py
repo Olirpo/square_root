0d9cb874f8557f711995b788321f4ef786797dc9
Olirpo <olvatgamer@gmail.com>
2021-06-06 21:09:53 -0300

    The main has been added

--- main.py
+++ main.py
@@ -7,15 +7,31 @@ def main():
         Welcome to the square root calculator
         Here you will able to use three different methods for the square root
         """)
-        option = input("""Please type your choose:
+        option = int(input("""Please type your choose:
         1. Brute force search
         2. Approach search
         3. Binary search
-        Your choose:
-        """)
+        
+        Your choose:"""))
 
         if option < 1 or option > 3:
             print('Please choose a correct option')
+
+        elif option == 1:
+            print('You choosed the brute force search, please type a number')
+            functions.brute_force(int(input('Your number: ')))
+        elif option == 2:
+            print('You choosed the brute force search, please type a number')
+            number = int(input('Your number: '))
+            epsilon = float(input('Epsilon *if you dont know what is this just put 0,1* :'))
+            functions.approach_search(number, epsilon)
+        elif option == 3:
+            print('You choosed the binary search, pleas type a number')
+            number = int(input('Your number: '))
+            epsilon = float(input('Epsilon *if you dont know what is this just put 0,1* :'))
+            functions.binary_search(number, epsilon)
+
+
 
 
 
